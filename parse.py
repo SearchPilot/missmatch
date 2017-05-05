0ddf1bc2a128a80869f54329c63e089a3f182f05
Duncan Morris <duncan.morris@distilled.net>
2017-05-05 10:03:19 +0100

    Add args to allow tweaking of soup parser fallback

--- parse.py
+++ parse.py
@@ -9,11 +9,8 @@ import lxml.html.soupparser
 from utils import get_response
 from utils import slug_from_url
 
-USE_SOUP_THRESHOLD = 0.9
-USE_SOUP_FALLBACK = True
 
-
-def should_use_soup_fallback(old_body, new_body):
+def should_use_soup_fallback(old_body, new_body, soupthreshold):
     """ Return True If the body of the parsed page is significantly smaller
     than the original (a sign the lxml parsing has failed)
     """
@@ -22,14 +19,13 @@ def should_use_soup_fallback(old_body, new_body):
     new_length = len(new_body)
     fraction = float(new_length) / old_length
 
-    fallback = False
-    if USE_SOUP_FALLBACK and fraction < USE_SOUP_THRESHOLD:
-        fallback = True
+    print(new_length, old_length, fraction,
+          soupthreshold, fraction < soupthreshold)
 
-    return fallback
+    return fraction < soupthreshold
 
 
-def parse_response_soup(body, encoding, request_id):
+def parse_response_soup(body, encoding):
     """ Use beautiful soup to parse the response
 
     This parser is slower and only used as a fallback. It is better at handling
@@ -47,7 +43,7 @@ def parse_response_soup(body, encoding, request_id):
     return tree
 
 
-def parse_response(body, encoding):
+def parse_response(body, encoding, allow_soup, soupthreshold):
     """ Parse the response into an lxml tree """
 
     etree_parser = etree.HTMLParser(recover=True, encoding=encoding)
@@ -56,7 +52,7 @@ def parse_response(body, encoding):
     new_body = etree.tostring(
         tree.getroottree(), method='html', encoding=encoding)
 
-    if should_use_soup_fallback(body, new_body):
+    if allow_soup and should_use_soup_fallback(body, new_body, soupthreshold):
         print('Falling back to Soup Parser')
         tree = parse_response_soup(body, encoding)
 
@@ -69,12 +65,18 @@ if __name__ == '__main__':
         description="Parse the url with lxml and show the diff")
     parser.add_argument('url')
     parser.add_argument('--encoding', default='utf-8')
+    parser.add_argument('--nosoup', default=False, action='store_true')
+    parser.add_argument('--soupthreshold', default=0.9, type=float)
     args = parser.parse_args()
 
     response = get_response(args.url, {})
     original_body = response.body
 
-    tree = parse_response(body=original_body, encoding=args.encoding)
+    allow_soup = not args.nosoup
+    tree = parse_response(body=original_body,
+                          encoding=args.encoding,
+                          allow_soup=allow_soup,
+                          soupthreshold=args.soupthreshold)
     parsed_body = etree.tostring(tree, method='html', encoding=args.encoding)
 
     original_body_lines = original_body.decode('utf-8').splitlines()
